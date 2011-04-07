f650f552fb5edc376f02c5aa6b8e6dfd1009d5d7
emptykid <qiang0902@gmail.com>
2011-04-07 20:50:43 +0800

    aaaaaa

---
+++ src/xq.php
@@ -0,0 +1,26 @@
+<?php
+/***************************************************************************
+ * 
+ * Copyright (c) 2011 Baidu.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * @file xq.php
+ * @author xiaoqiang(zhengqianglong@baidu.com)
+ * @date 2011/04/07 20:48:41
+ * @version $Revision$ 
+ * @brief 
+ *  
+ **/
+
+
+
+
+
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 noet: */
+?>
